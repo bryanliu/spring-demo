1feb941054abcf1d0940c51349bc15e4101f234b
Bryan Liu <bry@tradeshift.com>
2021-04-09 16:03:40 +0800

    initlal mybatis

---
+++ coffeeshopmybatis/src/main/java/com/bry/coffeeshopmybatis/CoffeeshopmybatisApplication.java
@@ -0,0 +1,13 @@
+package com.bry.coffeeshopmybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeshopmybatisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeshopmybatisApplication.class, args);
+	}
+
+}
---
+++ coffeeshopmybatis/src/test/java/com/bry/coffeeshopmybatis/CoffeeshopmybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bry.coffeeshopmybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeshopmybatisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
