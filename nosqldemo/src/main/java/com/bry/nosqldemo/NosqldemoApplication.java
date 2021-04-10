0b380033becfb72584a050fc906922749a4db568
Bryan Liu <bry@tradeshift.com>
2021-04-10 15:41:14 +0800

    initial nosql project

---
+++ nosqldemo/src/main/java/com/bry/nosqldemo/NosqldemoApplication.java
@@ -0,0 +1,13 @@
+package com.bry.nosqldemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NosqldemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NosqldemoApplication.class, args);
+	}
+
+}
---
+++ nosqldemo/src/test/java/com/bry/nosqldemo/NosqldemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bry.nosqldemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NosqldemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
