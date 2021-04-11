8b7483f6e02d653b3256974bb54465ed2de6358c
Bryan Liu <bry@tradeshift.com>
2021-04-11 16:44:35 +0800

    打开 Cacheable

--- coffeeshopjpa/src/main/java/com/bry/coffeeshopjpa/service/CoffeeService.java
+++ coffeeshopjpa/src/main/java/com/bry/coffeeshopjpa/service/CoffeeService.java
@@ -57,7 +57,7 @@ public class CoffeeService {
         coffeeRepository.deleteByName(name);
     }
 
-    //@Cacheable
+    @Cacheable()
     public List<Coffee> getAllCofffees() {
         return coffeeRepository.findAll();
     }
