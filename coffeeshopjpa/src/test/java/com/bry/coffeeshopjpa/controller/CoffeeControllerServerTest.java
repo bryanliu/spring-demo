094b6955277b7893243a93e9a74218824fc07a36
Bryan Liu <bryan.jh.liu@qq.com>
2021-04-13 16:15:16 +0800

    将 url + port 的初始化放到BeforeAll 方法中
    不过要做一些trick，TestInstance要设成PRE_CLASS。

--- coffeeshopjpa/src/test/java/com/bry/coffeeshopjpa/controller/CoffeeControllerServerTest.java
+++ coffeeshopjpa/src/test/java/com/bry/coffeeshopjpa/controller/CoffeeControllerServerTest.java
@@ -3,21 +3,21 @@ package com.bry.coffeeshopjpa.controller;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.containsString;
 import static org.hamcrest.Matchers.equalTo;
-import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.BDDMockito.given;
 
 import java.util.Arrays;
 import java.util.List;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.data.web.JsonPath;
 
 import com.bry.coffeeshopjpa.model.Coffee;
 import com.bry.coffeeshopjpa.service.CoffeeService;
@@ -31,19 +31,29 @@ import lombok.extern.slf4j.Slf4j;
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Slf4j
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class CoffeeControllerServerTest {
 
+    static String url = "http://localhost:";
     //是TestRestTemplate, 不是RestTemplate
     @Autowired TestRestTemplate rest;
     @MockBean CoffeeService coffeeService;
-    String url = "http://localhost:";
     @LocalServerPort
-    private Integer port;
+    Integer port;
     //不能这么写：String url = "http://localhost:"+port; 这个时候port还没初始化
 
-    @BeforeEach
+    @BeforeAll
+    /*
+      终于解决了，beforeAll 只能是静态方法，导致不能初始化 url + port，
+      port 是运行期注入的，但是在静态方法里面没法引用到 以及 和 url 组合起来
+     */
     void beforeAll() {
         url = url + port;
+    }
+
+    @BeforeEach
+    void beforeEach() {
+        // Run before every test cases.
     }
 
     @Test
@@ -59,7 +69,8 @@ public class CoffeeControllerServerTest {
         assertThat(res, containsString("c1"));
         assertThat(res, containsString("c2"));
         assertThat("c1", equalTo("c1"));
-        List<Coffee> c = new Gson().fromJson(res, new TypeToken<List<Coffee>>(){}.getType());
+        List<Coffee> c = new Gson().fromJson(res, new TypeToken<List<Coffee>>() {
+        }.getType());
         assertThat(c.size(), is(2));
         log.info(c.toString());
         // .i(Matchers.containsString("c1"));
