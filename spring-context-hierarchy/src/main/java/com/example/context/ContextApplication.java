0f8d3579fff8f707bab9284795379567ed766ea7
Bryan Liu <bryan.jh.liu@qq.com>
2021-04-13 20:36:48 +0800

    初始化 Spring context hierarchi

---
+++ spring-context-hierarchy/src/main/java/com/example/context/ContextApplication.java
@@ -0,0 +1,13 @@
+package com.example.context;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContextApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContextApplication.class, args);
+	}
+
+}
