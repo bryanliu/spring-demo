83b167577c46a9d74752c74e81aa314fd9757c13
Bryan Liu <bryan.jh.liu@qq.com>
2021-04-14 10:54:31 +0800

    将Service中的repo mock 掉，不直接调用数据库。

--- coffeeshopjpa/src/test/java/com/bry/coffeeshopjpa/service/CoffeeServiceTest.java
+++ coffeeshopjpa/src/test/java/com/bry/coffeeshopjpa/service/CoffeeServiceTest.java
@@ -3,7 +3,13 @@ package com.bry.coffeeshopjpa.service;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.times;
 
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,32 +18,67 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.bry.coffeeshopjpa.model.Coffee;
+import com.bry.coffeeshopjpa.repository.CoffeeRepository;
 
 @SpringBootTest
 public class CoffeeServiceTest {
+
+    @MockBean
+    private CoffeeRepository coffeeRepo;
 
     @Autowired
     private CoffeeService coffeeService;
 
     @Test
     public void testGotCoffeeByPriceExists() {
+        //arrange mock
+        Coffee c = Coffee.builder().name("espresso").price(10).createTime(new Date()).build();
+        given(coffeeRepo.findOne(any())).willReturn(Optional.ofNullable(c));
+
+        //invoke 1
         Optional<Coffee> coffee = coffeeService.getCoffeeByName("espresso");
 
+        //Capture and verify 1
+        then(coffeeRepo).should(times(1)).findOne(any());
         assertTrue(coffee.isPresent());
-
         assertEquals("espresso", coffee.get().getName());
+
+        //invoke 2
+        coffee = coffeeService.getCoffeeByName("espresso");
+
+        //Capture and verify 2
+        //invoke time still once, since get from cache, not from repo
+        then(coffeeRepo).should(times(1)).findOne(any());
     }
 
     @Test
     public void testHitCacheAndClearCache() {
+
+        //arrange mock
+        Coffee c = Coffee.builder().name("espresso").price(10).createTime(new Date()).build();
+        given(coffeeRepo.findOne(any())).willReturn(Optional.ofNullable(c));
+
+        //invoke
         Optional<Coffee> coffee1 = coffeeService.getCoffeeByName("espresso"); //No cache, will goto db
-        Optional<Coffee> coffee2 = coffeeService.getCoffeeByName("latte");//No cache, will goto db
-        coffee1 = coffeeService.getCoffeeByName("espresso"); //cached
+
+        //verify
+        then(coffeeRepo).should(times(1)).findOne(any());
+
+        //Got from cache
+        coffee1 = coffeeService.getCoffeeByName("espresso");
+        then(coffeeRepo).should(times(1)).findOne(any());
+
+        //Remove from cache
         coffeeService.remove("espresso");
-        coffee1 = coffeeService.getCoffeeByName("espresso"); //No cache, will goto db
-        coffee2 = coffeeService.getCoffeeByName("latte"); //cached
+
+        coffee1 = coffeeService.getCoffeeByName("espresso");
+        then(coffeeRepo).should(times(2)).findOne(any());
+
+        //cleanup
+        coffeeService.remove("espresso");
     }
 
     @Test
@@ -48,10 +89,23 @@ public class CoffeeServiceTest {
          * 改成Redis之后10000行明显慢了很多，大概要12s左右，一方面是本地的redis 性能有限，网络开销也不可忽视。
          * 而且之前都是内存数据库，内存缓存，所以速度会快很多。
          */
+        List<Coffee> coffeeList = Arrays.asList(
+                Coffee.builder().name("coffee1").price(20).build(),
+                Coffee.builder().name("coffee2").price(30).build()
+        );
+        given(coffeeRepo.findAll()).willReturn(coffeeList);
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
             List<Coffee> coffees = coffeeService.getAllCofffees();
         }
+
+        then(coffeeRepo).should(times(1)).findAll();
+
+    }
+
+    @Nested
+    @DisplayName("Cache Test")
+    class CacheTest {
 
     }
 
@@ -60,8 +114,12 @@ public class CoffeeServiceTest {
     class NegativeCase {
         @Test
         public void testGetCoffeeByPriceNotExists() {
+            //arrange mock
+            given(coffeeRepo.findOne(any())).willReturn(Optional.empty());
 
             Optional<Coffee> coffee = coffeeService.getCoffeeByName("aa");
+
+            then(coffeeRepo).should(times(1)).findOne(any());
 
             assertFalse(coffee.isPresent());
         }
