8f53e40284b1498f1a401f3847cf844861492b44
Bryan Liu <bryan.jh.liu@qq.com>
2021-04-25 13:35:07 +0800

    解决 config service 运行期加载不到 profile 配置文件中的配置。
    新建一个项目，用来测试性能

---
+++ spring-boot-performance/src/test/java/com/bryan/springbootperformance/SpringBootPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bryan.springbootperformance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
